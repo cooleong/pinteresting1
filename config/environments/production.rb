24ad03355c8032fc25c292f36663deaad676f3e2
John Lee <leongkahlee@gmail.com>
2016-01-09 00:25:10 +0800

    implemented devise for user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required by heroku
+  config.action_mailer.default_url_options = { host: 'http://cooleong-pinteresting1.herokuapp.com/' }
 end
